7307f0a89cf41449600330ac403cb08e4f4bc38e
AyeshaNaaz17 <ayeshanaazn17@gmail.com>
2022-12-31 00:23:50 +0530

    Commit - 2

--- src/OOPs/OopsBasic_1.java
+++ src/OOPs/OopsBasic_1.java
@@ -20,6 +20,14 @@ public class OopsBasic_1 {
         System.out.println(s1.name);
         System.out.println(s1.age);
         System.out.println(s1.cgpa);
+
+
+        BankAccount b1 = new BankAccount();
+        b1.name = "Def";
+//        b1.password = "adsfgh"; - gives error as password is set to private, cannot modify it outside BankAccount class
+        b1.setPassword("werttyuijnbv"); // sets the password to this as the operation of assigning password is done in the BankAccount class itself
+//        System.out.println(b1.setPassword("ss"));
+//        System.out.println(b1.password); - cannot print it because password is declared as private
     }
 }
 
@@ -43,5 +51,15 @@ class Student{
 
     void calcCgpa(int phy, int chem, int math){
         cgpa = (phy + chem + math) / 3;
+    }
+}
+
+
+class BankAccount{
+    public String name; // accessible outside the class
+    private String password; // only accessible in this class
+
+    void setPassword(String pwd){
+        password = pwd;
     }
 }
